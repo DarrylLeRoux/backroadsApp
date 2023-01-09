cd9c227359e256f58e0de5d2fa135c955c22ea75
Darryl Le Roux <devdarryl@outlook.com>
2023-01-09 13:53:56 +0200

    Added new hero text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,7 @@ const Hero = () => {
     <section className='hero' id='home'>
       <div className='hero-banner'>
         <h1>Backroads App</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-          explicabo debitis est autem dicta.
-        </p>
+        <p>We provide an array of luxury hiking trips around the world</p>
         <a href='#tours' className='btn hero-btn'>
           explore tours
         </a>
